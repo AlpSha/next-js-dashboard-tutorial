56cecfa7566fef98b385be5acee7340587824275
Alp Sahin <alp@sahin.club>
2024-12-03 15:34:22 +0300

    Deleting page param on search to show first page of new search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,6 +11,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
+    params.delete('page');
     if (term) {
       params.set('query', term);
     } else {
